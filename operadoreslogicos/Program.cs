45db3ff6a7933b40dba7a698eece5f5e1a53e375
RHEYLANDER SOARES RODRIGUES <rheylander.srodrigue@senacsp.edu.br>
2025-11-06 14:58:10 -0300

    enviando atualização

---
+++ operadoreslogicos/Program.cs
@@ -0,0 +1,12 @@
+﻿//Operadores Lógicos
+bool x = true;
+bool y = false;
+
+bool and = x && y; // E lógico
+bool or = x || y; // Ou lógico
+bool not = !x; // Não lógico
+
+Console.WriteLine($"\nOperadores Lógicos:");
+Console.WriteLine($"x && y: {and}"); // E lógico
+Console.WriteLine($"x || y: {or}"); // Ou lógico
+Console.WriteLine($"!x: {not} "); // Não lógico
---
+++ operadoresrelacionais/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 10;
+int b = 5;
+
+//Operadores Relacionais
+bool maiorQue = a > b;
+bool menorQue = a < b;
+bool igual = a == b;
+bool diferente = a != b;
+bool maiorOuIgual = a >= b;
+bool menorOuIgual = a <= b;
+
+//Exibindo resultados dos operadores relacionais
+Console.WriteLine($"{a} > {b}; {maiorQue}");
+Console.WriteLine($"{a} < {b}: {menorQue}:");
+Console.WriteLine($"{a} == {b}: {igual}");
+Console.WriteLine($"{a} != {b}: {diferente}");
+Console.WriteLine($"{a} >= {b}: {maiorOuIgual}");
+Console.WriteLine($"{a} <= {b} {menorOuIgual}");

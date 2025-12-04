c42753231512c452369848fd539275ab54a6977f
RHEYLANDER SOARES RODRIGUES <rheylander.srodrigue@senacsp.edu.br>
2025-12-04 16:48:49 -0300

    classes e listas

---
+++ classecomheranca/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica14
+{
+    //Classe base (superclasse)
+    internal class Animal
+    {
+        // Propriedade da classe Animal
+        public string Nome { get; set; } // Atributo
+
+        //Método que será sobrecarregado nas classes derivadas
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine("O animal emite um som.");
+        }
+    }
+
+    //Classe derivada (subclasse)
+    internal class Cachorro : Animal
+    {
+        //Sobrescrevendo o método EmitirSom
+        public override void EmitirSom()
+        {
+            Console.WriteLine($"{Nome} late: Au Au!");
+        }
+    }
+
+
+    //Outra classe derivada
+    internal class Gato : Animal
+    {
+        // Sobrescrevendo o método EmitirSom
+        public override void EmitirSom()
+        {
+            Console.WriteLine($"{Nome} mia: Miau!");
+        }
+
+    }
+
+
+    //  Programa Executável : 
+
+    internal class Program
+    {
+
+        // Sempre faço a instância dentro do Programa    
+        static void Main(string[] args)
+        {
+            // Criando instâncias das classes derivadas
+            Animal meuCachorro = new Cachorro { Nome = "Rex" };
+            Animal meuGato = new Gato { Nome = "Felix" };
+            Animal meuCoelho = new Animal { Nome = "Lulu" };
+
+
+            // Chamando o método EmitirSom para cada animal
+            meuCachorro.EmitirSom(); // Saída: Rex late: Au Au!
+            meuGato.EmitirSom();     // Saída: Felix mia: Miau!
+
+
+            // Utilizando Polimorfismo: Array de animais
+            Animal[] animais = { meuCachorro, meuGato, meuCoelho };
+
+            foreach (var animal in animais)
+
+            {
+                animal.EmitirSom(); // Cada objeto chama o método adequado de sua Classe
+
+
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ listas/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+
+    static void Main()
+    {
+        List<string> nomes = new List<string>();
+
+        //Adicionando elementos
+        nomes.Add("Ana");
+        nomes.Add("Bruno");
+        nomes.Add("Carlos");
+
+        Console.WriteLine(nomes[0]); //Ana
+        Console.WriteLine(nomes[1]); //Bruno
+        Console.WriteLine(nomes[2]); //Carlos
+
+        //Console.WriteLine("\n");
+
+        Console.WriteLine("\n Lista após adicionar elementos:");
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+
+        // Adiciona novo nome
+        nomes.Add("Daniel");
+
+        Console.WriteLine("\nLista após adicionar 'Daniel': ");
+        foreach (string nome in nomes)
+            {
+            Console.WriteLine(nome);
+            }
+
+        //Removendo Elementos
+        nomes.Remove("Bruno"); //remove pelo valor
+
+        Console.WriteLine("\nLista após remover 'Bruno':");
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+
+        //Removendo pelo índice
+        nomes.RemoveAt(0); // remove o primeiro (Ana)
+
+        Console.WriteLine("\nLista após remover o índice 0 (Ana):");
+        foreach (string nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+
+
+        //Limpando toda a lista
+        nomes.Clear();
+
+        Console.WriteLine("\nLista após Clear():");
+        Console.WriteLine("Quantidade de elementos: " + nomes.Count);
+    }
+
+}
